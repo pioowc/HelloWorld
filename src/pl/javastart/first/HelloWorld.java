e6663b7435014db1f543714da3e943af37bc48f9
pioowc <pio.owczarek@gmail.com>
2019-04-15 16:26:12 +0200

    pierwszy commit

---
+++ src/pl/javastart/first/HelloWorld.java
@@ -0,0 +1,52 @@
+package pl.javastart.first;
+
+import java.io.*;
+
+public class HelloWorld {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World \n Podaj liczbę: ...");
+
+		HelloWorld h = new HelloWorld();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String s = reader.readLine();
+		int i = Integer.parseInt(s);
+		System.out.printf("Liczba to %d .%n", i);
+
+		System.out.println("Wynik dla " + i + " = " + h.fibo(i));
+
+		System.out.println("\nWynik2 dla " + i + " = " + h.fibo2(i));
+
+	}
+
+	private int fibo(int n) {
+		int res = -1;
+
+		if (n == 0 || n == 1)
+			return n;
+
+		if (n >= 2)
+			return fibo(n - 1) + fibo(n - 2);
+
+		return res;
+	}
+
+	private int fibo2(int n) {
+
+		if (n == 0 || n == 1)
+			return n;
+		int a = 0, b = 1;
+		int suma = -1;
+
+		for (int i = 2; i <= n; i++) {
+			suma = a + b;
+			a = b;
+			b = suma;
+		}
+
+		return suma;
+	}
+
+}
